0ba2c55653e0071bc2aea48874f4052205e29686
unknown <gayoyawalkar@gmail.com>
2021-10-01 07:19:37 +0530

    Added:Daily Emp Wage Uc2

---
+++ EmpWageDailyEmpWage.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.empwage;
+//UC2
+public class EmpWageDailyEmpWage {
+	public static void main(String[] args) {
+		calculateDailyEmpWage();
+	}
+	public static void calculateDailyEmpWage() {
+		int is_full_time = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (is_full_time == empCheck) {
+			int empRatePerHour=20;
+			int empHrs=8;
+			double dailyEmpWage=(empHrs*empRatePerHour);
+			System.out.println("Daily Employee Wage if Employee is Present="+dailyEmpWage);
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+}
